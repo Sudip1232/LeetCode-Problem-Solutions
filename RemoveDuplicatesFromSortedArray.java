473bfa9dfdf7d9e926834d9e7e58a2e81563989e
Sudip Ghosh <146701736+Sudip1232@users.noreply.github.com>
2026-04-30 01:59:05 +0530

    Added solution for LeetCode 26 (Remove Duplicates)
    ## Approach 2 (Optimal)
    - Two pointer technique
    - Time: O(n), Space: O(1)

--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -21,3 +21,22 @@ class Solution {
     }
 }
 
+## Approach 2 (Optimal)
+- Two pointer technique
+- Time: O(n), Space: O(1)
+
+    
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+        int j=0;
+        for(i=1; j<nums.length;j++){
+            if(nums[j] != nums[i-1]){   
+            nums[i]=nums[j];
+            i++;
+                
+            }
+    }
+    return i;
+}
+}
